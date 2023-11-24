229f4941129f7a048dd17485871e220c3b2e24ca
akshay kathore <akshay@docintosh.com>
2023-11-24 16:53:55 +0530

    Add workflow

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
-import App from "./App";
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "./index.css";
 
+/* 
+Header
+    -logo
+    -Nav Items
+
+    Body
+        -Search
+        -RestaurantContainer
+        -RestaurantCard
+
+    Footer
+        -Copyright
+        -Links
+        -Address
+        -Contact
+*/
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return <div className="app"></div>;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(parent);
